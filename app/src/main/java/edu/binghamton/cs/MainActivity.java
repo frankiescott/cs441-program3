9aa275477e46c23d6e5f7291a616c2f14aeaa582
Frankie <email@frankiescott.net>
2019-06-20 15:04:53 -0400

    implemented temp conversion methods

--- app/src/main/java/edu/binghamton/cs/MainActivity.java
+++ app/src/main/java/edu/binghamton/cs/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //initialize edit text handlers
         EditText fahrenheit = findViewById(R.id.fahrenheit);
         EditText celsius = findViewById(R.id.celsius);
@@ -19,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         //supply default values
         fahrenheit.setText("32");
         celsius.setText("0");
+    }
+
+    public int ftc(int n) {
+        return (int) (n - 32) * (5/9);
+    }
+
+    public int ctof(int n) {
+        return n * (9/5) + 32;
     }
 }
