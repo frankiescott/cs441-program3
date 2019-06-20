85945dcd164416b740354de80ae5d512ca307ddd
Frankie <email@frankiescott.net>
2019-06-20 15:01:06 -0400

    added code for edit text handlers and provided default text values

--- app/src/main/java/edu/binghamton/cs/MainActivity.java
+++ app/src/main/java/edu/binghamton/cs/MainActivity.java
@@ -2,6 +2,7 @@ package edu.binghamton.cs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //initialize edit text handlers
+        EditText fahrenheit = findViewById(R.id.fahrenheit);
+        EditText celsius = findViewById(R.id.celsius);
+
+        //supply default values
+        fahrenheit.setText("32");
+        celsius.setText("0");
     }
 }
