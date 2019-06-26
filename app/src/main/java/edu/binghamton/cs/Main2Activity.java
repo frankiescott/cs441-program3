c67dc02b855b5e457870ead6877a720257273c0f
Frankie <email@frankiescott.net>
2019-06-26 12:03:50 -0400

    coded the done button to bring user back to main activity

--- app/src/main/java/edu/binghamton/cs/Main2Activity.java
+++ app/src/main/java/edu/binghamton/cs/Main2Activity.java
@@ -1,7 +1,10 @@
 package edu.binghamton.cs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
@@ -19,5 +22,19 @@ public class Main2Activity extends AppCompatActivity {
             TextView currentTemp = findViewById(R.id.currenttemp);
             currentTemp.setText("The current temperature is " + c + " degrees celsius");
         }
+
+        configureBackButton();
+    }
+
+    public void configureBackButton() {
+        Button backButton = findViewById(R.id.backbutton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
