4a53c67b2f9425e306fba4cc01a87aef1a14fdd9
Frankie <email@frankiescott.net>
2019-06-26 11:58:08 -0400

    celsius value is passed to second activity to display on screen

--- app/src/main/java/edu/binghamton/cs/Main2Activity.java
+++ app/src/main/java/edu/binghamton/cs/Main2Activity.java
@@ -2,12 +2,22 @@ package edu.binghamton.cs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Integer c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            int c = extras.getInt("c");
+            TextView currentTemp = findViewById(R.id.currenttemp);
+            currentTemp.setText("The current temperature is " + c + " degrees celsius");
+        }
     }
 }
